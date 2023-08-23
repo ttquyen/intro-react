3e7568db76bfc99c28598410ca0f4e9e398019fb
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-08-23 14:04:47 +0700

    add todolist html and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1 class="title">
+        TODO List
+        <span>Get one item done at a time</span>
+      </h1>
+      <ul class="task-list">
+        <li>
+          <span className="label">JS</span>
+          <div className="action-box">
+            <input type="checkbox" className="btn-action btn-action-done" />
+            <button className="btn-action btn-action-remove">✖</button>
+          </div>
+        </li>
+        <li>
+          <span className="label">JS</span>
+          <div className="action-box">
+            <input type="checkbox" className="btn-action btn-action-done" />
+            <button className="btn-action btn-action-remove">✖</button>
+          </div>
+        </li>
+      </ul>
+      <div className="filter-wrapper">
+        <label htmlFor="filter" className="filter-label">
+          Show incompleted tasks only
+        </label>
+        <input type="checkbox" className="btn-action btn-filter" id="filter" />
+      </div>
+      <form action="#" className="form">
+        <label htmlFor="new-item">Add to the TODO list</label>
+        <input type="text" id="new-item" />
+        <button className="btn-add" type="submit">
+          ADD ITEM
+        </button>
+      </form>
     </div>
   );
 }
