4bb29914d27c035fa7c8a79e0567fecda02d64cd
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-08-23 15:39:38 +0700

    control state React

--- src/App.js
+++ src/App.js
@@ -2,37 +2,94 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [taskList, setTaskList] = useState([
+    { id: "task_1", title: "Learn JS", status: 1 },
+    { id: "task_2", title: "Code TODO list level 2", status: 0 },
+  ]);
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTaskList([...taskList, task]);
+      setNewTask("");
+    }
+  };
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+  const handleShowIncomplete = (e) => {
+    setShowIncomplete(e.target.checked);
+  };
+  const setTaskStatus = (taskID, taskStatus) => {
+    setTaskList(
+      taskList.map((task) => {
+        if (task.id === taskID) {
+          return { ...task, status: taskStatus ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+  const removeTask = (taskID) => {
+    setTaskList(taskList.filter((task) => task.id !== taskID));
+  };
+
   return (
-    <div class="container">
-      <h1 class="title">
+    <div className="container">
+      <h1 className="title">
         TODO List
         <span>Get one item done at a time</span>
       </h1>
-      <ul class="task-list">
-        <li>
-          <span className="label">JS</span>
-          <div className="action-box">
-            <input type="checkbox" className="btn-action btn-action-done" />
-            <button className="btn-action btn-action-remove">✖</button>
-          </div>
-        </li>
-        <li>
-          <span className="label">JS</span>
-          <div className="action-box">
-            <input type="checkbox" className="btn-action btn-action-done" />
-            <button className="btn-action btn-action-remove">✖</button>
-          </div>
-        </li>
+      <ul className="task-list">
+        {taskList
+          .filter((task) => (showIncomplete ? task.status !== 1 : true))
+          .map((task) => (
+            <li key={task.id} className={task.status ? "done" : ""}>
+              <span className="label">{task.title}</span>
+              <div className="action-box">
+                <input
+                  type="checkbox"
+                  className="btn-action btn-action-done"
+                  onChange={(e) => setTaskStatus(task.id, e.target.checked)}
+                  checked={Boolean(task.status)}
+                />
+                <button
+                  className="btn-action btn-action-remove"
+                  onClick={() => removeTask(task.id)}
+                >
+                  ✖
+                </button>
+              </div>
+            </li>
+          ))}
       </ul>
       <div className="filter-wrapper">
         <label htmlFor="filter" className="filter-label">
           Show incompleted tasks only
         </label>
-        <input type="checkbox" className="btn-action btn-filter" id="filter" />
+        <input
+          type="checkbox"
+          className="btn-action btn-filter"
+          id="filter"
+          checked={showIncomplete}
+          onChange={handleShowIncomplete}
+        />
       </div>
-      <form action="#" className="form">
+      <form onSubmit={handleSubmit} className="form">
         <label htmlFor="new-item">Add to the TODO list</label>
-        <input type="text" id="new-item" />
+        <input
+          type="text"
+          id="new-item"
+          value={newTask}
+          onChange={handleInputChange}
+        />
         <button className="btn-add" type="submit">
           ADD ITEM
         </button>
